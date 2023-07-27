a68b02db5fbd0d280172fb19d7a11eab841903c5
Talani Balogun <Taloni5992@gmail.com>
2023-07-27 14:47:22 -0400

    src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,15 @@ import  SignUpForm  from './components/SignUpForm.jsx';
 import Authenticate  from './components/Authenticate';
 import './App.css'
 
-
 function App() {
-  const [token, setToken] = useState(null);
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
+  
+
   return (
     <>
-      <SignUpForm token={token} setToken={setToken} />
-      <Authenticate token={token} setToken={setToken} />
+      <Authenticate />
+      <SignUpForm />
     </>
   );
 }
+
 export default App
