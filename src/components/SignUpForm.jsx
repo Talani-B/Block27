0da966a26a6d01135dd8edc5ea09f3fc9ec326ff
Talani Balogun <Taloni5992@gmail.com>
2023-07-27 15:34:59 -0400

    update all files

--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -1,67 +1,56 @@
-import { useState } from 'react';
-import '../App.css'
+import { useState } from "react";
 
-
-export default function SignUpForm() { 
-
+export default function SignUpForm() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState(null);
 
-  
-
-async function handleSubmit(event) {
+  async function handleSubmit(event) {
     event.preventDefault();
-console.log(username);
-console.log(password);
-setUsername('');
-setPassword('');
-   try {
-    const response = await fetch(`{$BASE_URL}`,
+
+
+    try {
+          const response = await fetch('https://fsa-jwt-practice.herokuapp.com/signup', 
               { 
-                method: 'POST', 
+                method: "POST", 
                 headers: { 
-                  "Content-Type": 'application/json' 
+                  "Content-Type": "application/json" 
                 }, 
                 body: JSON.stringify({ 
-                  username: username, 
-                  password: password, 
+                  username: "some-username", 
+                  password: "super-secret-999" 
                 }) 
               })
-
+          const data =  await response.json()
+          console.log(data);
     } catch (error) {
       setError(error.message);
     }
-    
-    
   }
-    return ( 
-    
-    <div>
-       <h2>Sign Up!</h2>
-       {error && <p>{error}</p>}
-       <form onSubmit={handleSubmit}>
-        <label htmlFor='username'>Username</label>
+
+  return (
+    <>
+      <h2>Sign Up</h2>
+      {error && <p>{error}</p>}
+      <form onSubmit={handleSubmit}>
+        <label>
+          Username:{" "}
           <input
-            value={username} // controls the input value
-            type="username"
-            id="username"
-            onChange={(e) => {
-              console.log(e.target.value);
-              setUsername(e.target.value)
-            }}
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
           />
-        <label htmlFor='password'>Password</label>
+        </label>
+        <label>
+          Password:{" "}
           <input
-            value={password}
             type="password"
-            id="password"
-            onChange={(e) => {
-              console.log(e.target.value);
-              setPassword(e.target.value)
-             }}
-             />
-        <button type ="submit">Sign Up</button>
-    </form>
-    </div>
-    )}
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        
+        </label>
+        <button>Submit</button>
+      </form>
+    </>
+  );
+}
